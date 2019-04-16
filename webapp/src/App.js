1996ded7301a02988312aaeadfae2ef1dbffad2b
Shadowninja33 <prestondcobb@gmail.com>
2019-04-15 19:23:15 -0500

    New idea. Removed old table stuff

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import ParkingTable from './Components/parkingTable'
 
 class App extends Component {
+
+  setGarage = garage => {
+    this.setState({currentgarage: garage});
+  }
+
   render() {
     return (
       <div className="App">
