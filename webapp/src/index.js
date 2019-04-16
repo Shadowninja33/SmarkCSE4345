5b69afca0d7f11a660b088dbbc5f15e447c9bdc3
Shadowninja33 <prestondcobb@gmail.com>
2019-04-16 13:34:40 -0500

    Added BorswerRouter

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 
 import "bootstrap/dist/css/bootstrap.css";
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
