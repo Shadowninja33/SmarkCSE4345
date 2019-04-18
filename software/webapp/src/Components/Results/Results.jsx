216441c5246490cc3ed5ec14b6ea723fe65e029e
Shadowninja33 <38512435+Shadowninja33@users.noreply.github.com>
2019-04-17 23:57:35 -0500

    Changed Results to extend Component and added getSpaces call to componentDidMount()

--- software/webapp/src/Components/Results/Results.jsx
+++ software/webapp/src/Components/Results/Results.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { SpaceRepository } from '../../api';
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,64 +8,50 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { faCarSide } from '@fortawesome/free-solid-svg-icons';
 
 
-export const Results = (props) => {
+class Results extends React.Component {
+    spaceRepository = new SpaceRepository();
 
-    const columns = [{
-        Header: '#',
-        accessor: 'number',
-        minWidth: 30
-    }, {
-        Header: 'Garage',
-        accessor: 'garage',
-        minWidth: 150
-    }, {
-        Header: 'Level',
-        accessor: 'level',
-        minWidth: 50
-    }, {
-        Header: 'Compact',
-        accessor: 'compact',
-        minWidth: 50
-    }, {
-        Header: 'Covered',
-        accessor: 'covered',
-        minWidth: 50
-    }, {
-        Header: 'EV',
-        accessor: 'ev',
-        minWidth: 30
-    }]
+    state = {
+        spaces: [],
+    }
 
-    return (
-        <>
-            <Table className="table-light">
-                <Thead>
-                    <Tr>
-                        <Th>#</Th>
-                        <Th>Garage</Th>
-                        <Th>Level</Th>
-                        <Th>Compact</Th>
-                        <Th>Covered</Th>
-                        <Th>EV</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {
-                        props.data.map((a, i) =>
-                            <Tr key={i}>
-                                <Td>{a.number}</Td>
-                                <Td>{a.garage}</Td>
-                                <Td>{a.level}</Td>
-                                <Td>{(a.compact ? <FontAwesomeIcon icon={faCarSide} size="xl" style={{color: 'black'}} /> : <FontAwesomeIcon icon={faCarSide} size="xl" style={{color: 'lightgrey'}} /> )}</Td>
-                                <Td>{(a.covered ? <FontAwesomeIcon icon={faHome} size="xl" style={{color: 'black'}} /> : <FontAwesomeIcon icon={faHome} size="xl" style={{color: 'lightgrey'}} />)}</Td>
-                                <Td>{(a.ev ? <FontAwesomeIcon icon={faPlug} size="xl" style={{color: 'green'}} /> : <FontAwesomeIcon icon={faPlug} size="xl" style={{color: 'lightgrey'}} /> )}</Td>
-                            </Tr>
-                        )
-                    }
-                </Tbody>
-            </Table>
-        </>
-    );
+    render() {
+        return (
+            <>
+                <Table className="table-light">
+                    <Thead>
+                        <Tr>
+                            <Th>#</Th>
+                            <Th>Garage</Th>
+                            <Th>Level</Th>
+                            <Th>Compact</Th>
+                            <Th>Covered</Th>
+                            <Th>EV</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {
+                            props.data.map((a, i) =>
+                                <Tr key={i}>
+                                    <Td>{a.number}</Td>
+                                    <Td>{a.garage}</Td>
+                                    <Td>{a.level}</Td>
+                                    <Td>{(a.compact ? <FontAwesomeIcon icon={faCarSide} size="xl" style={{ color: 'black' }} /> : <FontAwesomeIcon icon={faCarSide} size="xl" style={{ color: 'lightgrey' }} />)}</Td>
+                                    <Td>{(a.covered ? <FontAwesomeIcon icon={faHome} size="xl" style={{ color: 'black' }} /> : <FontAwesomeIcon icon={faHome} size="xl" style={{ color: 'lightgrey' }} />)}</Td>
+                                    <Td>{(a.ev ? <FontAwesomeIcon icon={faPlug} size="xl" style={{ color: 'green' }} /> : <FontAwesomeIcon icon={faPlug} size="xl" style={{ color: 'lightgrey' }} />)}</Td>
+                                </Tr>
+                            )
+                        }
+                    </Tbody>
+                </Table>
+            </>
+        );
+    }
+
+    componentDidMount() {
+        this.spaceRepository.getSpaces()
+        .then(spaces => this.setState({ spaces }));
+    }
 }
 
 export default Results;
