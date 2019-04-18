3a9b8e491778da458d71badbf70448aa4dbacbd5
awkimball <addison@akimball.com>
2019-04-18 10:44:46 -0500

    add spots table

--- software/db/server.sql
+++ software/db/server.sql
@@ -4,6 +4,18 @@ use parking;
 CREATE TABLE lots (
 
     lotid INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32),
+    lat FLOAT,
+    long FLOAT,
+    PRIMARY KEY (lotid)
+
+);
+
+CREATE TABLE spots (
+
+    spotid INT NOT NULL AUTO_INCREMENT,
+    level int NOT NULL DEFAULT 1,
+    spotnum INT NOT NULL,
     
 
 
